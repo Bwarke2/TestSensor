3f335ffd05e32146e9709b6669b764d79b019e80
Bjarke <201805703@post.au.dk>
2021-05-17 10:19:40 +0200

    Started project. Added TempSensor

---
+++ TestSensor/TempSensor/ITempSensor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TempSensor
+{
+    public class TempEventArg : EventArgs
+    {
+        public double temp { set; get; }
+    }
+    public interface ITempSensor
+    {
+        event EventHandler<TempEventArg> TempChangedEvent;
+        double Read();
+        void Update(double temp);
+    }
+}
---
+++ TestSensor/TempSensor/TempSensor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TempSensor
+{
+	public class TempSensor : ITempSensor
+	{
+		public double _curretTemp = 0;    //Id of sensor
+		public event EventHandler<TempEventArg> TempChangedEvent;
+
+		public void OnTempChangedEvent()
+		{
+			TempChangedEvent?.Invoke(this, new TempEventArg() { temp = _curretTemp });
+		}
+
+		public void Update(double inputTemp = -272)
+        {
+			if(inputTemp <= -273)
+            {
+				Console.WriteLine("Error below abs. zero");
+            }
+			else
+            {
+				_curretTemp = inputTemp;
+            }
+        }
+
+		public double Read()
+        {
+			return _curretTemp;
+        }
+
+		public void SimulateTempChanged()
+        {
+			OnTempChangedEvent();
+        }
+
+	}
+
+}
